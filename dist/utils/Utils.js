5b9617cda00df302fa94deadfa02ffd2eaca406c
Adam Ferla <aferla@gmail.com>
2021-07-10 20:32:54 +1000

    Removed dist folder

--- dist/utils/Utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toUpperCase = void 0;
-function toUpperCase(input) {
-    return input.toUpperCase();
-}
-exports.toUpperCase = toUpperCase;
